b96dc9e902c840d5ea007e663504532c0cf74b32
Alexander Wu <hgalexwu@hotmail.com>
2017-02-19 02:31:27 -0500

    Added a new JS script that allows us to clear a specific column or an entire row using jQuery

---
+++ js/script.js
@@ -0,0 +1,47 @@
+
+// NEED TO CHECK IF ACTUALLY CLEARS THE WHOLE COLUMN
+
+
+// Clear column depending on type of column to clear
+function clearColumn(col) {
+	// Clear Student ID's column
+	if (col === 'sid') {
+		// jQuery selector that iterates through each td with name attribute = "sid" and empties td
+		$('[name="sid"]').each(function() {
+			$(this).html('');
+		});
+	}
+	// Clear Last Name column
+	else if (col === 'ln') {
+		// jQuery selector that iterates through each td with name attribute = "ln" and empties td
+		$('[name="ln"]').each(function() {
+			$(this).html('');
+		});
+	}
+	// Clear First Name column
+	else if (col === 'fn') {
+		// jQuery selector that iterates through each td with name attribute = "fn" and empties td
+		$('[name="fn"]').each(function() {
+			$(this).html('');
+		});
+	}
+	// Clear Grade column
+	else if (col === 'grade') {
+		// jQuery selector that iterates through each td with name attribute = "grade" and empties td
+		$('[name="grade"]').each(function() {
+			$(this).html('');
+		});
+	}
+	else {
+		alert("Invalid Column Type");
+	}
+}
+
+// !!! need to figure out a way to clear a specific row after dynamically adding rows
+function clearRow() {
+	// Empty each row's cells content
+	$('#secondrow [name="sid"]').html('');
+	$('#secondrow [name="ln"]').html('');
+	$('#secondrow [name="fn"]').html('');
+	$('#secondrow [name="grade"]').html('');
+}
